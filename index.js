dbf43071f2fea6623fe49521bf24b6d66c2e12fb
rohitkr0807 <rohitkr0807@gmail.com>
2020-01-25 13:44:28 +0530

    First node completed successfully

--- index.js
+++ index.js
@@ -2,3 +2,17 @@ var rect = {
   perimeter : (x,y) => (2*(x+y)),
   area : (x,y) => (x*y)
 }
+function solveRect(l,b) {
+  console.log("Solving for Rectangle lenght =  " + l + " " + "breadth = " + b);
+  if (l < 0 || b < 0)
+  {
+    console.log("Invalid length or breadth");
+  }
+  else {
+    console.log("The area of Rectangle is " + rect.area(l,b));
+    console.log("perimeter of Rectangle is " + rect.perimeter(l,b));
+  }
+}
+solveRect(2,4)
+solveRect(3,5)
+solveRect(-3,7)
