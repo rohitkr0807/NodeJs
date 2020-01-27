47b0789b90c38c42bc582c5768d035eb97bc9cf3
rohitkr0807 <rohitkr0807@gmail.com>
2020-01-27 22:16:42 +0530

    Error Handling done

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
-var rect = {
-  perimeter : (x,y) => (2*(x+y)),
-  area : (x,y) => (x*y)
-}
+var rect = require('./rectangle');
+
 function solveRect(l,b) {
   console.log("Solving for Rectangle lenght =  " + l + " " + "breadth = " + b);
-  if (l < 0 || b < 0)
-  {
-    console.log("Invalid length or breadth");
+    rect(l,b,(err,rectangle) => {
+      if (err)
+      {
+        console.log("error", err.message);
+      }else {
+        console.log("the area", rectangle.area, rectangle.perimeter);
+      }
+    });
+
   }
-  else {
-    console.log("The area of Rectangle is " + rect.area(l,b));
-    console.log("perimeter of Rectangle is " + rect.perimeter(l,b));
-  }
-}
 solveRect(2,4)
 solveRect(3,5)
 solveRect(-3,7)
--- rectangle.js
+++ rectangle.js
@@ -1,4 +1,16 @@
-
+module.exports = (x,y,callback) => {
+  if (x<0 || y<0)
+  {
+    setTimeout(() => callback(new Error("Invalid length or breadth"), null),2000)
+    // console.log("Rectangle is not possible");
+  }else {
+    setTimeout(() =>
+      null, {perimeter : (x,y) => (2*(x+y)),
+              area: (x,y) => (x*y)
+            }
+    ),2000
+  }
+}
 
 
 
