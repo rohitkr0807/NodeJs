a6d67175dfd6f12d7f871bff68794d30d64025ed
rohitkr0807 <rohitkr0807@gmail.com>
2020-01-27 23:30:18 +0530

    Fixes

--- rectangle.js
+++ rectangle.js
@@ -4,18 +4,9 @@ module.exports = (x,y,callback) => {
     setTimeout(() => callback(new Error("Invalid length or breadth"), null),2000)
     // console.log("Rectangle is not possible");
   }else {
-    setTimeout(() =>
-      null, {perimeter : (x,y) => (2*(x+y)),
-              area: (x,y) => (x*y)
-            }
-    ),2000
+    setTimeout(() => callback(null, {
+      area : () => (x*y),
+      perimeter: () => (2*(x+y))
+    }),2000);
   }
 }
-
-
-
-
-
-
-perimeter = (x,y) => (2*(x+y));
-area = (x,y) => (x*y);
